1ddd8530209ef7b70176b39a6b7cded954aa8874
Safa Demirhan <safademirhan88@gmail.com>
2024-07-29 17:07:04 +0300

    Added api app and removed hello app.

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('hello/', views.HelloWorld.as_view(), name='hello_world'),
 ]
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
+    path('api/', include('api.urls')),
 ]
