ab6ba62895a1186dd31135c147499f3be78f8c87
Safa Demirhan <safademirhan88@gmail.com>
2024-07-29 17:41:12 +0300

    Init Neo4j models.

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from neomodel import StructuredNode, StringProperty, DateTimeProperty, RelationshipTo
 
-# Create your models here.
+class User(StructuredNode):
+    distinguished_name = StringProperty(unique_index=True)
+    object_sid = StringProperty()
+    service_principal_name = StringProperty()
+    nt_security_descriptor = StringProperty()
+    when_created = DateTimeProperty()
+    # Define a relationship to the Group node
+    member_of = RelationshipTo('Group', 'MEMBER_OF')
+
+class Computer(StructuredNode):
+    distinguished_name = StringProperty(unique_index=True)
+    object_sid = StringProperty()
+    service_principal_name = StringProperty()
+    nt_security_descriptor = StringProperty()
+    when_created = DateTimeProperty()
+
+class Group(StructuredNode):
+    distinguished_name = StringProperty(unique_index=True)
+    object_sid = StringProperty()
+    service_principal_name = StringProperty()
+    nt_security_descriptor = StringProperty()
+    when_created = DateTimeProperty()
+    # Define a relationship to the User node
+    members = RelationshipFrom('User', 'MEMBER_OF')
