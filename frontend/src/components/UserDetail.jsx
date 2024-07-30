2efb91c65b30efe02ddd3597d8e1132a5403c034
Safa Demirhan <72216810+safademirhan9@users.noreply.github.com>
2024-07-30 08:44:27 -0700

    Init React app for frontend. (#4)

---
+++ frontend/src/components/UserDetail.jsx
@@ -0,0 +1,5 @@
+const UserDetail = () => {
+  return <div>User Detail</div>;
+};
+
+export default UserDetail;
---
+++ frontend/src/pages/UsersPage.jsx
@@ -0,0 +1,12 @@
+import UserList from '../components/UserList';
+
+const UsersPage = () => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <UserList />
+    </div>
+  );
+};
+
+export default UsersPage;
