a0d83604efeffe94ad04e3900edca3a576534e57
Safa Demirhan <safademirhan88@gmail.com>
2024-07-30 17:42:59 +0300

    Updated serializers to implement update function.

--- myproject/api/serializers.py
+++ myproject/api/serializers.py
@@ -11,6 +11,24 @@ class UserSerializer(serializers.Serializer):
     def create(self, validated_data):
         return User(**validated_data).save()
 
+<<<<<<< Updated upstream
+=======
+    def update(self, instance, validated_data):
+        instance.object_sid = validated_data.get('object_sid', instance.object_sid)
+        instance.service_principal_name = validated_data.get(
+            'service_principal_name', instance.service_principal_name
+        )
+        instance.nt_security_descriptor = validated_data.get(
+            'nt_security_descriptor', instance.nt_security_descriptor
+        )
+        instance.when_created = validated_data.get(
+            'when_created', instance.when_created
+        )
+        instance.save()
+        return instance
+
+
+>>>>>>> Stashed changes
 class ComputerSerializer(serializers.Serializer):
     distinguished_name = serializers.CharField(max_length=255)
     object_sid = serializers.CharField(max_length=255)
@@ -21,6 +39,24 @@ class ComputerSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Computer(**validated_data).save()
 
+<<<<<<< Updated upstream
+=======
+    def update(self, instance, validated_data):
+        instance.object_sid = validated_data.get('object_sid', instance.object_sid)
+        instance.service_principal_name = validated_data.get(
+            'service_principal_name', instance.service_principal_name
+        )
+        instance.nt_security_descriptor = validated_data.get(
+            'nt_security_descriptor', instance.nt_security_descriptor
+        )
+        instance.when_created = validated_data.get(
+            'when_created', instance.when_created
+        )
+        instance.save()
+        return instance
+
+
+>>>>>>> Stashed changes
 class GroupSerializer(serializers.Serializer):
     distinguished_name = serializers.CharField(max_length=255)
     object_sid = serializers.CharField(max_length=255)
@@ -31,3 +67,19 @@ class GroupSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Group(**validated_data).save()
 
+<<<<<<< Updated upstream
+=======
+    def update(self, instance, validated_data):
+        instance.object_sid = validated_data.get('object_sid', instance.object_sid)
+        instance.service_principal_name = validated_data.get(
+            'service_principal_name', instance.service_principal_name
+        )
+        instance.nt_security_descriptor = validated_data.get(
+            'nt_security_descriptor', instance.nt_security_descriptor
+        )
+        instance.when_created = validated_data.get(
+            'when_created', instance.when_created
+        )
+        instance.save()
+        return instance
+>>>>>>> Stashed changes
